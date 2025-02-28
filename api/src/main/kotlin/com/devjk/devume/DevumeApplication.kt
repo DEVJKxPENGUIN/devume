23dd52b9cca242a6c0b14961f07928c2101c370a
soheehong <zohizohi@naver.com>
2025-02-28 16:48:08 +0900

    feat ddohee/ant build gradle project

---
+++ api/src/main/kotlin/com/devjk/devume/DevumeApplication.kt
@@ -0,0 +1,11 @@
+package com.devjk.devume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevumeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevumeApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/devjk/devume/DevumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devjk.devume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevumeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
