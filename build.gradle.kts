764018535f014a585a5fa7c59e62247f1387e87d
devjk <rmsghk4254@naver.com>
2025-08-05 23:14:03 +0900

    feat(api): deploy 세팅 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,10 +45,8 @@ subprojects {
         implementation("io.grpc:grpc-services")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.springframework.grpc:spring-grpc-spring-boot-starter")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-        testImplementation("org.springframework.grpc:spring-grpc-test")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 
         // test
--- devume-client/server/api/health.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event): HealthCheckResponse => {
-  return new class implements HealthCheckResponse {
-    status: string = 'ok';
-  }
-})
-
-interface HealthCheckResponse {
-  'status'?: (string);
-}
