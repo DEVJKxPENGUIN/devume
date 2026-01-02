5091145e082ae44983a69773b1cbb1c3ef49b630
devjk <rmsghk4254@naver.com>
2026-01-02 23:53:25 +0900

    feat(client): background loop three js 적용

--- devume-client3/src/components/layout/BackgroundLoop.tsx
+++ devume-client3/src/components/layout/BackgroundLoop.tsx
@@ -1,29 +1,41 @@
 "use client";
 
-import {useEffect, useRef} from 'react';
 import styles from './BackgroundLoop.module.css';
+import ColorBends from "@/components/layout/ColorBlends";
 
 const BackgroundLoop = () => {
-  const videoRef = useRef<HTMLVideoElement>(null)
+  // const videoRef = useRef<HTMLVideoElement>(null)
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.6; // 2배 느리게 재생
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     videoRef.current.playbackRate = 0.6; // 2배 느리게 재생
+  //   }
+  // }, [])
 
   return (
       <div className={styles.background}>
-        <video
-            ref={videoRef}
-            src="/backgroundloop.mp4"
-            autoPlay
-            muted
-            loop
-            playsInline
-            className={styles.video}
+        {/*<video*/}
+        {/*    ref={videoRef}*/}
+        {/*    src="/backgroundloop.mp4"*/}
+        {/*    autoPlay*/}
+        {/*    muted*/}
+        {/*    loop*/}
+        {/*    playsInline*/}
+        {/*    className={styles.video}*/}
+        {/*/>*/}
+        <ColorBends
+            colors={["#ff5c7a", "#8a5cff", "#00ffd1"]}
+            rotation={0}
+            speed={0.2}
+            scale={1}
+            frequency={1.4}
+            warpStrength={1.2}
+            mouseInfluence={0.8}
+            parallax={0.6}
+            noise={0.08}
+            transparent
         />
-        <div className={styles.backdrop}></div>
+        {/*<div className={styles.backdrop}></div>*/}
       </div>
   )
 }
